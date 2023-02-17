a2a77ac494d26f299845362423be0e68a728c982
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2023-02-17 17:44:24 +0600

    test

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#bell").click(function(){
+        $("#drawer").toggle();
+    });
+});
+$(document).ready(function(){
+    $("#user-opt-btn").click(function(){
+        $("#user-opt-drawer").toggle();
+    });
+});
+$(document).ready(function(){
+    $(".sidebar-btn").click(function(){
+        $("#sidebar").toggleClass("sidebar-show")
+    });
+});
