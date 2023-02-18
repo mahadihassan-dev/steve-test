5046ab70ae680ed5f775fcbdf5489e1032c97536
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2023-02-18 18:42:21 +0600

    Responsive Issues about Overflow -fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,8 @@
-$(document).ready(function(){
+jQuery(document).ready(function(){
     $("#bell").click(function(){
         $("#drawer").toggle();
     });
-});
-
-$(document).ready(function(){
     $("#user-opt-btn").click(function(){
         $("#user-opt-drawer").toggle();
-    });
-});
-
-$(document).ready(function(){
-    $(".sidebar-btn").click(function(){
-        $("#sidebar").toggleClass("sidebar-show")
     });
 });
--- assets/plugins/sidebar/sidebarToggle.js
+++ assets/plugins/sidebar/sidebarToggle.js
@@ -1,57 +1,54 @@
-function SidebarBbtn(){
+// function arlo_tm_switcher(){
 	
-	"use strict";
+// 	"use strict";
+
+
 	
-	var switcherOpener	= jQuery('.sidebar-btn');
-	var switcherIcon	= jQuery('.arlo_tm_leftpart_wrap .arlo_tm_resize i');
-	var leftPart		= jQuery('.arlo_tm_leftpart_wrap');
-	var rightPart		= jQuery('.arlo_tm_rightpart');
 	
-	switcherOpener.on('click',function(){
-		if(switcherOpener.hasClass('opened')){
-			switcherOpener.removeClass('opened');
-			switcherIcon.removeClass('opened');
-			leftPart.removeClass('opened');
-			rightPart.removeClass('opened');
-		}else{
-			switcherOpener.addClass('opened');
-			switcherIcon.addClass('opened');
-			leftPart.addClass('opened');
-			rightPart.addClass('opened');
-		}
-		setTimeout(function(){jQuery('#ripple').ripples('updateSize');},101);
-		setTimeout(function(){jQuery('#ripple').ripples('updateSize');},201);
-		setTimeout(function(){jQuery('#ripple').ripples('updateSize');},301);
+// 	var switcherOpener				= jQuery('.sidebar-btn');
+// 	// var switcherIcon				= jQuery('.arlo_tm_leftpart_wrap .arlo_tm_resize i');
+// 	// var leftPart					= jQuery('.arlo_tm_leftpart_wrap');
+// 	// var rightPart					= jQuery('.arlo_tm_rightpart');
+	
+// 	switcherOpener.on('click',function(){
+// 		if(switcherOpener.hasClass('sidebar-show')){
+// 			switcherOpener.removeClass('sidebar-show');
+// 			// switcherIcon.removeClass('opened');
+// 			// leftPart.removeClass('opened');
+// 			// rightPart.removeClass('opened');
+// 		}else{
+// 			switcherOpener.addClass('sidebar-show');
+// 			// switcherIcon.addClass('opened');
+// 			// leftPart.addClass('opened');
+// 			// rightPart.addClass('opened');
+// 		}
 		
-		if(jQuery('.jarallax').length){
-			jQuery('.jarallax').jarallax('destroy');
-			setTimeout(function(){arlo_tm_jarallax();},300);
+// 		return false;
 		
-		}
-		return false;
-		
-	});
+// 	});
 	
 
-}
+// }
 
-function arlo_tm_jarallax(){
-	
-	"use strict";
-	
-	jQuery('.jarallax').each(function(){
-		var element			= jQuery(this);
-		var	customSpeed		= element.data('speed');
-		
-		if(customSpeed !== "undefined" && customSpeed !== ""){
-			customSpeed = customSpeed;
+$(document).ready(function () {
+	$(".sidebar-btn").click(function () {
+		$(".dash-main-left").toggleClass('showSidebar');
+		$(".dash-main-right ").toggleClass('full-width');
+		if($(".dash-main-left-inner").hasClass('show-left-inner')){
+			$(".dash-main-left-inner").removeClass('show-left-inner');
+			$(".dash-main-left-inner").addClass('hide-left-inner');
 		}else{
-			customSpeed 	= 0.5;
+			$(".dash-main-left-inner").addClass('show-left-inner');
+			$(".dash-main-left-inner").removeClass('hide-left-inner');
 		}
-		
-		element.jarallax({
-			speed: customSpeed,
-			automaticResize: true
-		});
+		if($(".dash-main-left").hasClass('showSidebar')){
+			$(".sidebar-btn").style.transform = "rotate(-180deg) !important";
+			$(".sidebar-btn").style.right = "-30px !important";
+		}
 	});
-}
+});
+
+// $(".sidebar-btn").click(function () {
+// 	$(".dash-main-left").toggleClass('showSidebar');
+// });
+  
