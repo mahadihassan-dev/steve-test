b0b15f50bcade20e93c4b7926f5f993be3788547
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2023-02-21 19:13:35 +0600

    Issues Tracker

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,38 @@
-jQuery(document).ready(function(){
-    $("#bell").click(function(){
+jQuery(document).ready(function () {
+    $("#bell").click(function () {
         $("#drawer").toggle();
     });
-    $("#user-opt-btn").click(function(){
+    $("#user-opt-btn").click(function () {
         $("#user-opt-drawer").toggle();
     });
 });
+
+
+
+// Sidebar - Toggle Button
+$(document).ready(function () {
+
+    $(".sidebar-btn").click(function () {
+        $(".dash-main-right").toggleClass('full-width');
+        $(".dash-main-left").toggleClass('hideSidebar');
+        $(".dash-main-left-inner").toggleClass('hideSidebar');
+        if ($(".dash-main-left-inner").hasClass('show-left-inner')) {
+            $(".dash-main-left-inner").removeClass('show-left-inner');
+        } else {
+            $(".dash-main-left-inner").hideClass('hide-left-inner');
+        }
+    })
+    
+    $(".sidebar_show_btn").click(function () {
+        $(".dash-main-right").toggleClass('full-width');
+        $(".dash-main-left").toggleClass('hideSidebar');
+        $(".dash-main-left-inner").toggleClass('hideSidebar');
+        if ($(".dash-main-left-inner").hasClass('show-left-inner')) {
+            $(".dash-main-left-inner").removeClass('show-left-inner');
+        } else {
+            $(".dash-main-left-inner").hideClass('hide-left-inner');
+        }
+    })
+
+});
+
